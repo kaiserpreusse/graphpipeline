94b15afac19d53b3c1d1ae125c065528b6872385
Martin <martin.preusse@gmail.com>
2021-03-05 16:49:11 +0100

    import to higher level

--- graphpipeline/datasource/__init__.py
+++ graphpipeline/datasource/__init__.py
@@ -1,3 +1,3 @@
 from .datasource import RollingReleaseRemoteDataSource, ManyVersionsRemoteDataSource, SingleVersionRemoteDataSource, \
-    BaseDataSource, DataSourceInstance
+    BaseDataSource, DataSourceInstance, download_latest_if_not_exists, download_latest
 from .datasourceversion import DataSourceVersion
