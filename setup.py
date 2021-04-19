de04efea282bf8719ab3dd0a8ab4da205b97d6b4
Martin <martin.preusse@gmail.com>
2021-04-19 14:20:20 +0200

    bump graphio dependency to 0.1.0

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(name='graphpipeline',
       license='MIT',
       packages=find_packages(),
       install_requires=[
-          'requests', 'ftputil', 'graphio', 'python-dateutil', 'bs4', 'webdavclient3'
+          'requests', 'ftputil', 'graphio>=0.1.0', 'python-dateutil', 'bs4', 'webdavclient3'
       ],
       keywords=['data'],
       zip_safe=False,
